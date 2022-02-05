fc61a666d704a5a0c180ab2a8393c392d5cba9e3
calebjenkins <caleb@calebjenkins.com>
2022-02-05 15:31:43 -0600

    sequence diagrams and devSetup v1

---
+++ dev-setup.bat
@@ -0,0 +1,10 @@
+cd .\src\Snippy.Web\ 
+dotnet user-secrets init
+
+:: Check if User Secrets have been created already
+
+:: Prompt for User Secrets and Set Them - skip if they are left blank
+
+:: Local DB Connection String
+:: OAuth Azure AD Client Key
+:: Oauth Azure AD Secret

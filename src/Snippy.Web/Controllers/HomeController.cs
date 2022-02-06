ed117ad549cd43e7fb72b4ab1ffa7b6d4af6560b
calebjenkins <caleb@calebjenkins.com>
2022-02-05 23:58:13 -0600

    removed old web.

---
+++ src/Snippy.Web/Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Snippy.Data;
+using Snippy.Models;
+using Snippy.Web.Models;
+using System.Diagnostics;
+using System.Security.Claims;
+
+namespace Snippy.Web.Controllers;
+
+[Authorize]
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly IData _data;
+    private readonly IActionContextAccessor _httpAccessor;
+
+    public HomeController(ILogger<HomeController> logger, IData SnippyData, IActionContextAccessor HttpAccessor)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _data = SnippyData ?? throw new ArgumentNullException(nameof(SnippyData));
+        _httpAccessor = HttpAccessor ?? throw new ArgumentNullException(nameof(HttpAccessor));
+    }
+
+    public IActionResult Index(string Id)
+    {
+        var identity = _httpAccessor?.ActionContext?.HttpContext.User.Identity as ClaimsIdentity; // Azure AD V2 endpoint specific
+        string? preferred_username = identity?.Claims?.FirstOrDefault(c => c.Type == "preferred_username")?.Value;
+
+
+        var model = new IndexViewModel()
+        {
+            Title = "Snippy Web | Main",
+            Platform = Environment.OSVersion.ToString(),
+            AuthenticatedUser = _data.GetOwner("hello"),
+            Message = $"-->{Id}<-- Welcome: {preferred_username}"
+        };
+
+        _logger.LogInformation($"Log Info from Index controller { DateTime.Now.ToString() }");
+
+        return View("Index", model);
+    }
+
+    [AllowAnonymous]
+    public IActionResult Short(string Id, string ExtraPath)
+    {
+        var httpIdentity = _httpAccessor?.ActionContext?.HttpContext.User.Identity as ClaimsIdentity;
+        string? preferred_username = httpIdentity?.Claims.FirstOrDefault(c => c.Type == "preferred_username")?.Value;
+        string? user_name = httpIdentity?.Claims?.FirstOrDefault(c => c.Type == "name")?.Value;
+        string? source_ip = _httpAccessor?.ActionContext?.HttpContext?.Connection?.RemoteIpAddress?.ToString();
+
+        var request = new ClickRequest()
+        {
+            ShortUrlId = Id,
+            IdentId = preferred_username,
+            SourceIp = source_ip
+        };
+
+        var data = _data.RegisterClick(request);
+        if (data == null)
+        {
+            return Index(Id);
+        }
+
+        var extraPathWithQuery = ExtraPath + HttpContext.Request.QueryString.Value;
+        var urlDivider = (data.Url.EndsWith('/')) ? string.Empty : "/";
+        var completeURL = (extraPathWithQuery.Length > 0) ? data.Url + urlDivider + extraPathWithQuery : data.Url;
+
+        var model = new IndexViewModel()
+        {
+            Title = "Snippy Web | Short",
+            Platform = Environment.OSVersion.ToString(),
+            AuthenticatedUser = _data.GetOwner(preferred_username),
+            Message = $"-->{Id} | {completeURL}<-- for: {user_name} ({preferred_username}) from IP {request.SourceIp}"
+        };
+
+        _logger.LogInformation($"Log Info from Shorty controller { DateTime.Now.ToString() }");
+
+        return View("Index", model);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
--- src/Snippy.Web/Program.cs
+++ src/Snippy.Web/Program.cs
@@ -1,25 +1,29 @@
 using FakeAuth;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc.Authorization;
-using Microsoft.Identity.Web;
+using FakeAuth.Profiles;
 using Microsoft.Identity.Web.UI;
+using Lamar.Microsoft.DependencyInjection;
+using Snippy.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
-//builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-//    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
-builder.Services.UseFakeAuth();
+
+builder.Host.UseLamar<DependencyRegistration>();
+
+// Authentication Schema
+BuildAuth(builder);
 
 builder.Services.AddAuthorization(options =>
 {
     // By default, all incoming requests will be authorized according to the default policy.
     options.FallbackPolicy = options.DefaultPolicy;
 });
+
 builder.Services.AddRazorPages()
     .AddMicrosoftIdentityUI();
+
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
@@ -42,4 +46,44 @@ app.UseAuthorization();
 app.MapRazorPages();
 app.MapControllers();
 
+app.UseEndpoints(endpoints =>
+{
+	//endpoints.MapHealthChecks("api/hc").RequireAuthorization();
+	// at some point I want to explore adding standard health checks to all my apps.
+	// https://github.com/Xabaril/AspNetCore.Diagnostics.HealthChecks#Tutorials,-demos-and-walkthroughs-on-ASP.NET-Core-HealthChecks
+
+
+	endpoints.MapDefaultControllerRoute();
+
+	endpoints.MapControllerRoute(name: "short",
+		pattern: "/{id}/{*ExtraPath}",
+		defaults: new { controller = "Home", action = "Short" }
+		);
+
+	endpoints.MapControllerRoute(
+						name: "default",
+						pattern: "/",
+						defaults: new { controller = "Home", action = "index" }
+						);
+
+	// Reserving now for future work...
+	endpoints.MapControllerRoute(
+						name: "api",
+						pattern: "api/{controller=Home}/{action=index}/{id?}"
+						);
+});
+
 app.Run();
+
+public partial class Program
+{
+	// Added so we can override this in integration tests
+	// Also added to csproj file: <InternalsVisibleTo Include="IntegrationTests" />
+	public static Action<WebApplicationBuilder> BuildAuth = (builder) =>
+	{
+		builder.Services.UseFakeAuth<AzureProfile>();
+		//builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+		// .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
+
+	};
+}
